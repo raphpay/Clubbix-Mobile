a01261d4097d1da861a75d22a27446cebb712894
raphpay <raphpay.dev@gmail.com>
2025-06-10 15:49:32 +0400

    Initial Commit

---
+++ Clubbix-Mobile/Clubbix_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clubbix_MobileApp.swift
+//  Clubbix-Mobile
+//
+//  Created by Raphaël Payet on 10/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Clubbix_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clubbix-MobileTests/Clubbix_MobileTests.swift
@@ -0,0 +1,16 @@
+//
+//  Clubbix_MobileTests.swift
+//  Clubbix-MobileTests
+//
+//  Created by Raphaël Payet on 10/06/2025.
+//
+
+import Testing
+
+struct Clubbix_MobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
