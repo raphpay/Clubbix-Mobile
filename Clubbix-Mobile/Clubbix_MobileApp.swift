1fc8951e12b6b201d7c072f71906907f9fb5365f
raphpay <raphpay.dev@gmail.com>
2025-06-10 16:19:12 +0400

    Order folders and files #1

--- Clubbix-Mobile/Clubbix_MobileApp.swift
+++ Clubbix-Mobile/Clubbix_MobileApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Clubbix_MobileApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+			LoginView()
         }
     }
 }
---
+++ Clubbix-Mobile/Presentation/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Clubbix-Mobile
+//
+//  Created by Raphaël Payet on 10/06/2025.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LoginView()
+}
