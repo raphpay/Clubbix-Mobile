4111e9b4300b941201cee37413d8851a9195d5a3
raphpay <raphpay.dev@gmail.com>
2025-06-10 17:23:33 +0400

    Feature: Handle custom colors #1

---
+++ Clubbix-Mobile/Presentation/Color+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Ext.swift
+//  Clubbix-Mobile
+//
+//  Created by Raphaël Payet on 10/06/2025.
+//
+
+import SwiftUI
+
+extension Color {
+	static let background = Color("Background-Light")
+	static let clubbixBorder = Color("Border")
+	static let clubbixError = Color("Error")
+	static let primary = Color("CustomPrimary")
+	static let secondary = Color("CustomSecondary")
+	static let clubbixSuccess = Color("Success")
+	static let clubbixTextSecondary = Color("Text-Secondary")
+}
---
+++ Clubbix-Mobile/Presentation/Components/ClubbixTextField.swift
@@ -0,0 +1,18 @@
+//
+//  ClubbixTextField.swift
+//  Clubbix-Mobile
+//
+//  Created by Raphaël Payet on 10/06/2025.
+//
+
+import SwiftUI
+
+struct ClubbixTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClubbixTextField()
+}
